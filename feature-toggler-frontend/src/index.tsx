fc59e7c0fd437dcc1aec4fad05aef5fbd90edeb0
Ioan Rosgrim <ioan.rosgrim@ingka.ikea.com>
2021-11-15 18:26:06 +0100

    get the feature toggles

--- feature-toggler-frontend/src/index.tsx
+++ feature-toggler-frontend/src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import CreateAccount from './components/CreateAccount';
 
-const createAccount = true;
+const createAccount = false;
 
 ReactDOM.render(
   <React.StrictMode>
